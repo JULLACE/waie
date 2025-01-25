77be3d2aaf4e61fbdd19fd9f3e48e55685705334
J <88823380+jijirepo@users.noreply.github.com>
2025-01-25 15:34:03 -0500

    DEATH WITH TESSERACT-NODE, LIFE TO TESSERACT.JS (refactored routers)

--- backend/controllers/tesseract.js
+++ backend/controllers/tesseract.js
@@ -1,44 +1,36 @@
 const tesRouter = require('express').Router()
-const tesseract = require('node-tesseract-ocr');
-
-// Tesseract configuration
-const config = {
-  lang: 'eng',  // Language of OCR (eng by default)
-  oem: 1,       // OCR Engine mode
-  psm: 3        // Page segmentation mode
-};
+const { createWorker } = require('tesseract.js');
 
 tesRouter.get('/', async (request, response) => {
-  // actually idk what a get would do here lol
-  // prob getting specific ingredient request
-  response.send('alive and well')
+  const worker = await createWorker('eng', 1, {
+    cachePath: './lang',
+  });
+
+  const { data: { text } } = await worker.recognize('https://tesseract.projectnaptha.com/img/eng_bw.png');
+  console.log('Processed:', text);
+  await worker.terminate();
+
+  response.send(`${text} \nand healthy!`)
 })
 
 tesRouter.post('/upload', async (request, response) => {
   if (!request.files || Object.keys(request.files).length === 0) {
-    return response.status(400).send('No files were uploaded.').end();
+    return response.status(400).send('No files were uploaded.');
   }
+
+  let langChoice = request.body.lang ? request.body.lang : 'eng'
+
+  const worker = await createWorker([`${langChoice}`], 1, {
+    cachePath: './lang',
+  });
 
   const gotImage = request.files.image
-  const langChoice = request.body.lang
 
-  if (langChoice) config.lang = langChoice
+  const { data: { text } } = await worker.recognize(gotImage.data);
+  console.log('Processed:', text);
+  await worker.terminate();
 
-  // Sends image buffer to tesseract for recognition and plaintext conversion 
-  try {
-    let scannedText = await tesseract.recognize(gotImage.data, config)
-    console.log('OCR Result...', scannedText)
-
-    response.status(201).send(scannedText) // THIS IS TEMPORARY
-
-  } catch (error) {
-    console.error('Error:', error.message);
-    response.status(500).send('An error occurred while processing the image.');
-  }
-
-  // then send to GPT for processing here ...
-  // if certain possible allergens are within the food,
-  // make it send a JSON in a specific strict format ??
+  response.send(text)
 })
 
 module.exports = tesRouter
