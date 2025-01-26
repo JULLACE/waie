cd4b0616cb5e19420b0550b0898c1319e9505531
J <88823380+jijirepo@users.noreply.github.com>
2025-01-26 03:39:42 -0500

    Ingredients can be explained (backend files were not pushed)

--- backend/utils/openaiService.js
+++ backend/utils/openaiService.js
@@ -10,15 +10,15 @@ async function getInfo(ingredient) {
             model: "gpt-4o-mini",
             messages: [{
                 "role": "user",
-                "content": "Write 1-2 concise sentences of what " + ingredient + " is."
+                "content": "Write only 1-2 concise sentences of what " + ingredient + " is. Be sure to include if they are harmful or not. Use the following JSON format: { explanation: '...' }"
             }],
             response_format: { "type": "json_object" },
-            max_tokens: 50,
         });
         
         const info = result.choices[0].message["content"];
-        console.log(info);
-        return info;
+        const content = JSON.parse(info);
+        console.log(content);
+        return content;
 
     } catch (error) {
         console.log(error);
@@ -60,7 +60,7 @@ async function getDietaryRestrictions(ingredientList) {
             model: "gpt-4o-mini",
             messages: [{
                 "role": "user", 
-                "content": `${ingredientList}\nList any possible allergies and dietary restrictions associated with the ingredients into an array, and then put this array into a JSON. Please keep it concise. For example, { 'restrictions': ['Peanut Allergy', 'Not Gluten Free', 'Vegan'] }. Account for as many restrictions as possible associated with the list.`
+                "content": `${ingredientList}\nList any possible allergies, dietary restrictions, or harmful effects associated with the ingredients into an array, and then put this array into a JSON. Please keep it concise. For example, { 'restrictions': ['Peanut Allergy', 'Not Gluten Free', 'Vegan'] }. Account for as many restrictions as possible associated with the list.`
             }],
             response_format: { "type": "json_object" },
         });
